84ca40855ae0447ee94ab8853531f3f591d8e1ac
littlej8 <95560628+littlej8@users.noreply.github.com>
2024-02-29 14:59:56 -0600

    pid controlled driving autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SmoothAutonomous.java
@@ -0,0 +1,174 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.Subsystems.*;
+import org.firstinspires.ftc.teamcode.StateSubsystems.Claw;
+
+public abstract class SmoothAutonomous extends LinearOpMode {
+    private DriveTrain drive;
+    private Trapdoor trap;
+    private Claw claw;
+    private TeamPropSensors sensors;
+
+    private boolean FarSide;
+    private boolean Red;
+    private double MoveUp;
+    private double LeftAdjust;
+    private double RightAdjust;
+    private double FrontAdjust1;
+    private double FrontAdjust2;
+    private double MoveBackStraight;
+    private double MoveBackLeft;
+    private double MoveBackRight;
+    private double ParkMove1Straight;
+    private double ParkMove1Strafe;
+    private double ParkMove2Straight = 0.0;
+    private double ParkMove2Strafe = 0.0;
+    private double ParkMove3Straight = 0.0;
+    private double ParkMove3Strafe = 0.0;
+
+    abstract public void on_init();
+
+    @Override
+    public void runOpMode() {
+        on_init();
+
+        initialize();
+
+        waitForStart();
+
+        // go up to team prop
+        drive.DriveForward(MoveUp, true);
+        sleep(500);
+
+        // check color sensors for where team prop is
+        String side = sensors.CheckSensors();
+        telemetry.addData("Side: ", side);
+        telemetry.update();
+        sleep(1000);
+        
+        // turn to pixel and adjust
+        if (side.equals("left")) {
+            drive.TurnTo(90);
+            sleep(500);
+
+            drive.DriveForward(LeftAdjust, true);
+        } else if (side.equals("right")) {
+            drive.TurnTo(-90);
+            sleep(500);
+
+            drive.DriveForward(RightAdjust, true);
+        } else {
+            drive.DriveLeft(FrontAdjust1, true);
+            sleep(500);
+
+            drive.DriveBackward(FrontAdjust2, true);
+        }
+        sleep(500);
+
+        // drop pixel and push onto line
+        trap.Open();
+        drive.DriveForward(25);
+        drive.DriveBackward(25);
+        sleep(1000);
+
+        // close trap turn back straight
+        trap.Close();
+        drive.TurnTo(0);
+        sleep(500);
+
+        // go back
+        drive.DriveBackward(MoveBackStraight, true);
+        sleep(500);
+
+        // turn to face board
+        drive.TurnTo((Red) ? -90 : 90);
+
+        // wait for alliance partner if far side
+        if (!FarSide) {
+            sleep(500);
+        } else {
+            sleep(10000);
+        }
+
+        // move up to board
+        drive.DriveForward(ParkMove1Straight, true);
+        sleep(500);
+
+        // line up with board
+        if (Red) {
+            drive.DriveLeft(ParkMove2Strafe / 2, true);
+        } else {
+            drive.DriveRight(ParkMove2Strafe / 2, true);
+        }
+        sleep(500);
+
+        // make sure still turned correctly
+        drive.TurnTo((Red) ? -90 : 90); 
+        sleep(500);
+
+        // move really slow to not ram board
+        drive.DriveForward(50, 0.05);
+        sleep(500);
+
+        // put arm on board
+        claw.MoveArmToGrab();
+        sleep(500);
+
+        // drop pixel
+        claw.DropClaw();
+        sleep(500);
+
+        // retract arm
+        claw.PullArmIn();
+    }
+
+    public void init_vars(boolean far_side, double move_up, double left_adjust, double right_adjust, double front_adjust_1, double front_adjust_2, double move_back_straight, double move_back_left, double move_back_right, double park_move_1_straight, double park_move_1_strafe) {
+        FarSide = far_side;
+        MoveUp = move_up;
+        LeftAdjust = left_adjust;
+        RightAdjust = right_adjust;
+        FrontAdjust1 = front_adjust_1;
+        FrontAdjust2 = front_adjust_2;
+        MoveBackStraight = move_back_straight;
+        MoveBackLeft = move_back_left;
+        MoveBackRight = move_back_right;
+        ParkMove1Straight = park_move_1_straight;
+        ParkMove1Strafe = park_move_1_strafe;
+
+        Red = (ParkMove1Straight > 0);
+    }
+
+    public void init_vars(boolean far_side, double move_up, double left_adjust, double right_adjust, double front_adjust_1, double front_adjust_2, double move_back_straight, double move_back_left, double move_back_right, double middle_move_1_straight, double middle_move_1_strafe, double middle_move_2_straight, double middle_move_2_strafe, double middle_move_3_straight, double middle_move_3_strafe) {
+        FarSide = far_side;
+        MoveUp = move_up;
+        LeftAdjust = left_adjust;
+        RightAdjust = right_adjust;
+        FrontAdjust1 = front_adjust_1;
+        FrontAdjust2 = front_adjust_2;
+        MoveBackStraight = move_back_straight;
+        MoveBackLeft = move_back_left;
+        MoveBackRight = move_back_right;
+        ParkMove1Straight = middle_move_1_straight;
+        ParkMove1Strafe = middle_move_1_strafe;
+        ParkMove2Straight = middle_move_2_straight;
+        ParkMove2Strafe = middle_move_2_strafe;
+        ParkMove3Straight = middle_move_3_straight;
+        ParkMove3Strafe = middle_move_3_strafe;
+
+        Red = (ParkMove1Straight > 0);
+    }
+
+    public void initialize() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        drive = new DriveTrain(this, hardwareMap, telemetry);
+        trap = new Trapdoor(hardwareMap);
+        claw = new Claw(hardwareMap);
+        sensors = new TeamPropSensors(hardwareMap);
+
+        claw.GripClaw();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveTrain.java
@@ -11,6 +11,9 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
+import com.arcrobotics.ftclib.controller.wpilibcontroller.ProfiledPIDController;
+import com.arcrobotics.ftclib.trajectory.TrapezoidProfile;
+
 @Config
 public class DriveTrain {
     private final LinearOpMode opMode;
@@ -19,6 +22,7 @@ public class DriveTrain {
     private final Telemetry telemetry;
     public static double DRIVE_SPEED = 0.2, TURN_SPEED = 0.1, TURN_TOLERANCE = 5.0;
     public static double TELEOP_SPEED = 0.3, TELEOP_TURN_MULT = 0.75;
+    public static double kP = 0.0, kI = 0.0, kD = 0.0, maxAccel = 0.05, pid_tolerance;
 
     public DriveTrain(LinearOpMode opMode, HardwareMap hwMap, Telemetry telemetry) {
         this.opMode = opMode;
@@ -67,24 +71,40 @@ public class DriveTrain {
         this.br.setPower((turn + (y - x)) * TELEOP_SPEED);
     }
 
-    public DriveForward(double amount, double power) {
-        drive(amount, amount, amount, amount, power)
-    }
-
     public void DriveForward(double amount) {
         drive(amount, amount, amount, amount, DRIVE_SPEED);
+    }
+
+    public void DriveForward(double amount, boolean smooth) {
+        drive_smooth(amount, amount, amount, amount, DRIVE_SPEED);
+    }
+
+    public void DriveForward(double amount, double power) {
+        drive(amount, amount, amount, amount, power)
     }
 
     public void DriveBackward(double amount) {
         drive(-amount, -amount, -amount, -amount, DRIVE_SPEED);
     }
 
+    public void DriveBackward(double amount, boolean smooth) {
+        drive_smooth(-amount, -amount, -amount, -amount, DRIVE_SPEED);
+    }
+
     public void DriveLeft(double amount) {
         drive(amount, amount, -amount, -amount, DRIVE_SPEED);
     }
 
+    public void DriveLeft(double amount, boolean smooth) {
+        drive_smooth(amount, amount, -amount, -amount, DRIVE_SPEED);
+    }
+
     public void DriveRight(double amount) {
         drive(-amount, -amount, amount, amount, DRIVE_SPEED);
+    }
+
+    public void DriveRight(double amount, boolean smooth) {
+        drive_smooth(-amount, -amount, amount, amount, DRIVE_SPEED);
     }
 
     public void TurnTo(double target) {
@@ -172,6 +192,44 @@ public class DriveTrain {
         this.fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         this.bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         this.br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    private void drive_smooth(double fl, double fr, double bl, double br, double power) {
+        double fl_target = this.fl.getCurrentPosition() + fl;
+        double fr_target = this.fr.getCurrentPosition() + fr;
+        double bl_target = this.bl.getCurrentPosition() + bl;
+        double br_target = this.br.getCurrentPosition() + br;
+
+        TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(power, maxAccel);
+        ProfiledPIDController fl_controller = new ProfiledPIDController(kP, kI, kD, constraints);
+        fl_controller.setGoal(fl_target);
+        fl_controller.setTolerance(pid_tolerance);
+        ProfiledPIDController fr_controller = new ProfiledPIDController(kP, kI, kD, constraints);
+        fr_controller.setGoal(fr_target);
+        fr_controller.setTolerance(pid_tolerance);
+        ProfiledPIDController bl_controller = new ProfiledPIDController(kP, kI, kD, constraints);
+        bl_controller.setGoal(bl_target);
+        bl_controller.setTolerance(pid_tolerance);
+        ProfiledPIDController br_controller = new ProfiledPIDController(kP, kI, kD, constraints);
+        br_controller.setGoal(br_target);
+        br_controller.setTolerance(pid_tolerance);
+
+        this.fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        while (opMode.opModeIsActive() && !(fl_controller.atSetpoint() && fr_controller.atSetpoint() && bl_controller.atSetpoint() && br_controller.atSetpoint())) {
+            this.fl.setPower(fl_controller.calculate((double)this.fl.getCurrentPosition()));
+            this.fr.setPower(fr_controller.calculate((double)this.fr.getCurrentPosition()));
+            this.bl.setPower(bl_controller.calculate((double)this.bl.getCurrentPosition()));
+            this.br.setPower(br_controller.calculate((double)this.br.getCurrentPosition()));
+        }
+
+        this.fl.setPower(0.0);
+        this.fr.setPower(0.0);
+        this.bl.setPower(0.0);
+        this.br.setPower(0.0);
     }
 
     public void ResetOrientation() {
